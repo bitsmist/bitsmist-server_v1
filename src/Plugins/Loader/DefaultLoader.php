e5e347b32b4adb939ff4e959db4f5792c09d14a5
Masaki Yasutake <my17560@gmail.com>
2021-11-13 19:23:00 +0900

    Modified to be able to specify a router class name.

--- src/Plugins/Loader/DefaultLoader.php
+++ src/Plugins/Loader/DefaultLoader.php
@@ -91,40 +91,18 @@ class DefaultLoader extends PluginBase
 	public function loadRoute(): ?array
 	{
 
+		$className = $this->options["container"]["settings"]["router"]["className"] ?? "nikic\FastRoute";
 		$routes = $this->options["container"]["settings"]["router"]["routes"];
 
-		$dispatcher = \FastRoute\simpleDispatcher(function(\FastRoute\RouteCollector $r) use ($routes) {
-			foreach ($routes as $routeName => $route)
-			{
-				$r->addRoute(['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], $route["route"], $routeName);
-			}
-		});
-
-		$routeinfo = $dispatcher->dispatch($_SERVER["REQUEST_METHOD"], $_SERVER["REQUEST_URI"]);
-
-		$args = null;
-		switch ($routeinfo[0])
+		$ret = null;
+		switch ($className)
 		{
-		case \FastRoute\Dispatcher::NOT_FOUND:
-			header("HTTP/1.1 404 OK\r\n");
-			throw new HttpException(HttpException::ERRNO_PARAMETER_INVALIDROUTE, HttpException::ERRMSG_PARAMETER_INVALIDROUTE);
-			break;
-		case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-			header("HTTP/1.1 405 OK\r\n");
-			throw new HttpException(HttpException::ERRNO_PARAMETER_INVALIDMETHOD, HttpException::ERRMSG_PARAMETER_INVALIDMETHOD);
-			break;
-		case \FastRoute\Dispatcher::FOUND:
-			$routeName = $routeinfo[1];
-			if (($routes[$routeName]["handler"] ?? "default") == "reject")
-			{
-				header("HTTP/1.1 " . ($routes[$routeName]["status"] ?? "404") . " OK\r\n");
-				exit;
-			}
-			$args = $routeinfo[2];
+		case "nikic\FastRoute":
+			$ret = $this->loadRoute_FastRoute($routes);
 			break;
 		}
 
-		return $args;
+		return $ret;
 
 	}
 
@@ -133,7 +111,7 @@ class DefaultLoader extends PluginBase
 	/**
 	 * Load services.
 	 */
-	function loadManagers()
+	public function loadManagers()
 	{
 
 		$container = &$this->options["container"];
@@ -445,6 +423,53 @@ class DefaultLoader extends PluginBase
 		}
 
 		return $ret;
+
+	}
+
+	// -----------------------------------------------------------------------------
+
+	/**
+  	 * Load route using nikic/FastRoute.
+	 *
+	 * @param	$routes			Routes.
+	 *
+	 * @return	Route arguments.
+     */
+	private function loadRoute_FastRoute($routes)
+	{
+
+		$dispatcher = \FastRoute\simpleDispatcher(function(\FastRoute\RouteCollector $r) use ($routes) {
+			foreach ($routes as $routeName => $route)
+			{
+				$r->addRoute(['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], $route["route"], $routeName);
+			}
+		});
+
+		$routeinfo = $dispatcher->dispatch($_SERVER["REQUEST_METHOD"], $_SERVER["REQUEST_URI"]);
+
+		$args = null;
+		switch ($routeinfo[0])
+		{
+		case \FastRoute\Dispatcher::NOT_FOUND:
+			header("HTTP/1.1 404 OK\r\n");
+			throw new HttpException(HttpException::ERRNO_PARAMETER_INVALIDROUTE, HttpException::ERRMSG_PARAMETER_INVALIDROUTE);
+			break;
+		case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+			header("HTTP/1.1 405 OK\r\n");
+			throw new HttpException(HttpException::ERRNO_PARAMETER_INVALIDMETHOD, HttpException::ERRMSG_PARAMETER_INVALIDMETHOD);
+			break;
+		case \FastRoute\Dispatcher::FOUND:
+			$routeName = $routeinfo[1];
+			if (($routes[$routeName]["handler"] ?? "default") == "reject")
+			{
+				header("HTTP/1.1 " . ($routes[$routeName]["status"] ?? "404") . " OK\r\n");
+				exit;
+			}
+			$args = $routeinfo[2];
+			break;
+		}
+
+		return $args;
 
 	}
 
